1fee838316ad7cfbb4db31eded044a07ec70c8e8
Cee-Jey <cebo.zuko@webmail.co.za>
2015-03-25 08:37:02 -0700

    added the file

---
+++ Cortex Hub Website/Javascript/Slider.js
@@ -0,0 +1,36 @@
+﻿var on = true;
+var myVar = setInterval(function () { if (on) { slides() } on = true; }, 5000);
+
+function slides(i) {
+    document.getElementsByTagName("span")[1].style.width = "0px";
+    document.getElementsByTagName("span")[2].style.width = "100%";
+    var slide = document.getElementsByTagName("span")[0];
+    document.getElementById("slide").appendChild(slide);
+    if (i) {
+        on = false;
+    }
+}
+function back_slide(i) {
+    document.getElementsByTagName("span")[1].style.width = "0px";
+    document.getElementsByTagName("span")[0].style.width = "100%";
+    var slide = document.getElementsByTagName("span")[4];
+    var slide2 = document.getElementsByTagName("span")[0];
+    document.getElementById("slide").insertBefore(slide, slide2);
+    if (i) {
+        on = false;
+    }
+}
+function is_touch_device() {
+    return !!('ontouchstart' in window)
+        || !!('onmsgesturechange' in window);
+};
+
+$(document).ready(function () {
+    if (is_touch_device()) {
+        $('p').unbind('mousenter mouseleave touchend touchstart');
+        $('p').bind('touchstart', function () {
+            $('p').removeClass('hover');
+            $(this).addClass('hover');
+        });
+    }
+});
